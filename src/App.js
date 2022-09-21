1cbef25464e4502aba77b18b120a2dcedf49d19e
lxMZK <alexmmizak@gmail.com>
2022-09-21 19:52:14 -0400

    First stub

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bookstore Library & Stock keeping app</h1>
+      <h4>coming soon . . .</h4>
+      <br />
+      <br />
+      <h3>Tavoy Walls / Alex Mizak / Quenton Powell</h3>
+    </div>
+  );
+}
+
+export default App;
