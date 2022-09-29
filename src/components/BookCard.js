f286b6adc0fdc6bee2716535a8e251853e0d9a27
lxMZK <alexmmizak@gmail.com>
2022-09-28 20:13:13 -0400

    initial css

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -5,14 +5,20 @@ export default function BookCard(props) {
     if (props.book) {
         return (
             <Card className='card'>
-                <Card.Img variant="top" src="https://media.wiley.com/product_data/coverImage300/82/15095467/1509546782.jpg" />
-                <Card.Body>
-                    <Card.Title>Title: {props.book.title}</Card.Title>
-                    <Card.Text>author: {props.book.author}</Card.Text>
-                    <Card.Text>Description . . .</Card.Text>
-                    <Card.Text>Rating: {props.book.rating}</Card.Text>
-                    <Card.Text>Price: {props.book.price}</Card.Text>
-                    <Card.Text>amount available: {props.book.amount_available}</Card.Text>
+                <div className='coverContainer'>
+                    <Card.Img className='bookCover' variant="top" src="https://media.wiley.com/product_data/coverImage300/82/15095467/1509546782.jpg" />
+                </div>
+                <Card.Body className='bookInfo'>
+                    <div className='bookContainer'>
+                        <Card.Title className='bookTitle'>{props.book.title}</Card.Title>
+                        <Card.Text>⭐{props.book.rating}</Card.Text>
+                    </div>
+                    <Card.Text className='bookAuthor'><i>{props.book.author}</i></Card.Text>
+                    <Card.Text className='bookDesc'>Description . . .</Card.Text>
+                    <div className='bookContainer'>
+                        <Card.Text><b>${props.book.price}</b></Card.Text>
+                        <Card.Text>{props.book.amount_available ? 'In Stock!' : 'Unavailable'}</Card.Text>
+                    </div>
                 </Card.Body>
             </Card>
         )
--- src/controllers/books_controller.js
+++ src/controllers/books_controller.js
@@ -11,6 +11,18 @@ books.post('/', async (req,res)=>{
     }
 })
 
+books.post('/new', async (req,res)=>{
+    try{
+        const newBook = await Book.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new book',
+            data: newBook
+        })
+    }  catch(err){
+        res.status(500).json(err)
+    }
+})
+
 books.get('/', async (req,res)=>{
     try {
         const foundBooks = await Book.findAll()
